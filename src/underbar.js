9187b119d0b818c5c245e7072397c451ba06452f
Brian Lowther <merchanthagen@gmail.com>
2021-09-21 11:15:16 -0700

    Implemented Reject test cases

--- src/underbar.js
+++ src/underbar.js
@@ -91,8 +91,10 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    var rejected = _.filter( collection, function(item) {
+      return !test(item);
+    })
+    return rejected;
   };
 
   // Produce a duplicate-free version of the array.
